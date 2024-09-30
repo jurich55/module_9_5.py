a6a5280d9589c514ca70984d0d498a556ac246b3
Vladimir <sv_urich@mail.ru>
2024-09-30 15:38:42 +0400

    Add files via upload

---
+++ module_9_5.py/main.py
@@ -0,0 +1,63 @@
+class StepValueError(ValueError):
+    pass
+
+
+class Iterator:
+    def __init__(self, start, stop, step=1):
+        self.start = start - step
+        self.stop = stop
+        self.step = step
+        self.pointer = start
+        if self.step == 0:
+            raise StepValueError('шаг не может быть равен 0')
+
+    def __iter__(self):
+        self.start, self.pointer, self.stop = self.start, self.start, self.stop
+        return self
+
+    def __next__(self):
+        self.pointer += self.step
+        if self.stop > 0 and self.step > 0:
+            if self.pointer > self.stop:
+                raise StopIteration()
+        elif self.stop < 0 and self.step > 0:
+            if self.pointer > self.stop:
+                raise StopIteration()
+        elif self.stop > 0 and self.step < 0:
+            if self.pointer < self.stop:
+                raise StopIteration()
+        else:
+            raise StopIteration()
+
+        return self.pointer
+
+
+try:
+    iter1 = Iterator(100, 200, 0)
+    for i in iter1:
+        print(i, end=' ')
+except StepValueError:
+    print('Шаг указан неверно')
+
+iter2 = Iterator(-5, 1)
+iter3 = Iterator(6, 15, 2)
+iter4 = Iterator(5, 1, -1)
+iter5 = Iterator(10, 1)
+
+for i in iter2:
+    print(i, end=' ')
+print()
+
+for i in iter3:
+    print(i, end=' ')
+print()
+
+for i in iter4:
+    print(i, end=' ')
+print()
+
+for i in iter5:
+    print(i, end=' ')
+print()
+
+
